11bb92607f72da044c5230fa16e2573b487f15c2
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2023-11-21 17:46:39 +0530

    Add files via upload

---
+++ do while/1 to n.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int x=1;
+	int n;
+	printf("enter the value of n:");
+	scanf("%d",&n);
+	do
+	{
+		printf("%d ",x);
+		x++;
+		
+	}while(x<=n);
+}
---
+++ do while/10 to 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+main()
+{
+	int x=1;
+	int n=10;
+	do
+	{	
+		printf("%d ",n);
+		n--;
+		
+	}while(n>=x);
+}
