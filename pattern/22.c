1fa3483bc2ac12ab4649ace93994374bcf7e14ff
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2023-11-28 18:58:02 +0530

    Add files via upload

---
+++ pattern/22.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{	
+//A
+//B C
+//D E F
+//G H I J
+//K L M N O
+   	int i,j;
+	char x='A';
+    for(i=1;i<=5;i++)
+    {
+    	for(j=1;j<=i;j++)
+    	{
+    		printf("%c ",x);
+    		x++;
+		}
+		printf("\n");
+	}
+}
