92336757fde85ff9a973e487f26102ed23b1500b
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2023-12-05 20:16:22 +0530

    Add files via upload

---
+++ 2 pattern/space pattern/4.c
@@ -0,0 +1,22 @@
+//        5
+//      5 4
+//    5 4 3
+//  5 4 3 2
+//5 4 3 2 1
+#include<stdio.h>
+main()
+{
+	int i,j,k; 
+	for(i=5; i>=1; i--)
+	{
+//		for(k=i; k>1; k--)
+//		{
+//			printf(" ");
+//		}
+		for(j=i; j>=1; j--)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+}
