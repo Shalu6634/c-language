33332e361e38a7a22712789478177f0c697531cf
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2023-12-11 07:22:04 +0530

    Add files via upload

---
+++ 1D array/array insert.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+	int n,x,i;
+	printf("enter the value of n:");
+	scanf("%d",&n);
+	int a[n];
+	
+	for(i=0; i<n; i++)
+	{
+        printf("enter the a[%d]:", i);
+        scanf("%d",&a[i]);
+	}
+	printf("enter the value of x:");
+	scanf("%d",&x);
+	a[n]=x;
+	for(i=0; i<n; i++)
+	{
+	   printf("%d",a[i]);
+	}
+}
