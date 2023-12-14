5f66645814e06caeb330336d09d447314a15ac74
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2023-12-14 06:52:04 +0530

    Add files via upload

---
+++ Assortment/2frequency.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+main()
+{
+	int n;
+	printf("enter size of array:");
+	scanf("%d",&n);
+	int a[n],b[n];
+	int i,j;
+	int x=1;
+	for(i=0; i<n; i++)
+	{
+		printf("enter a[%d]:",i);
+		scanf("%d",&a[i]);
+	}
+	for(i=0; i<n; i++)
+	{
+		for(j=i+1; j<n; j++)
+		{
+			if(a[i]==a[j])
+			{
+				x++;
+				a[j]=0;
+			}
+		}
+		b[i]=x;
+		if(a[i]!=0)
+		{
+			printf("%d -> %d\n",a[i],b[i]);
+		}
+		x=1;
+	}
+}
