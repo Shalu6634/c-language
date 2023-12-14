1ad3f50bb6b2b449607d8af074554b8cfb77501d
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2023-12-14 07:35:10 +0530

    Add files via upload

---
+++ string2/remove space,blanks.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+	int a[50];
+	printf("enter your name:");
+	int i;
+	for(i=0;i<50; i++)
+	{
+		scanf(" %c",&a[i]);
+		if(a[i]==' ')
+		{
+			a[i]=0;
+		}
+		if(a[i]!=0)
+	    {
+		  printf("%c",a[i]);
+	    }
+	}
+}
+
