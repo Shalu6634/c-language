8362fe71f87c21c28701565632cf3114b876e0a7
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2023-12-15 17:56:09 +0530

    Add files via upload

---
+++ if & else if/(x+y)/(x+y)3.c
@@ -0,0 +1,12 @@
+//1.WAP to find a the answer given formula (x+y)3.
+#include<stdio.h>
+main()
+{
+	int x,y,ans;
+	
+	printf("enter the value of x,y:");
+	scanf("%d%d",&x,&y);
+	
+	ans=(x*x*x) +(y*y*y)+ (3*x*x*y) +(3*x*y*y) ;
+	printf("ans is:%d",ans); 
+}
---
+++ if & else if/(x+y)/(x+y+z)3.c
@@ -0,0 +1,13 @@
+//1.WAP to find a the answer given formula (x+y+z)3.
+#include<stdio.h>
+main()
+{
+	int x,y,z,ans;
+	
+	printf("enter the value of x,y,z:");
+	scanf("%d%d",&x,&y,&z);
+	
+	ans=(x*x*x)+(y*y*y)+(z*z*z)-(3*x*y*z)+(x+y+z)+(x*x*y*z*z-x*y-y*z-z*x);
+	
+	printf("ans is:%d",ans); 
+}
