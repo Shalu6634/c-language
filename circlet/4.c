a03eb17b91f7d1a21585cc0c7997752caedf2b64
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2023-12-16 08:02:14 +0530

    Add files via upload

--- circlet/4.c
+++ circlet/4.c
@@ -1,21 +1,18 @@
 #include<stdio.h>
 main()
 {
-	int i,j,k;
-	for(i=1; i<=5; i++)
-	{
-		for(j=5; j>=i; j--)
-		{
-		
-			if(j%2==0)
-			{
-				printf("0");
-			}
-			else
-			{
-				printf("1");
-			}
-		}
-		printf("\n");
-	}
+//1
+//2 1
+//3 2 1
+//4 3 2 1
+//5 4 3 2 1
+  int i,j;
+  for(i=1; i<=5;i++)
+  {
+  	 for(j=i; j>=1; j--)
+  	  {
+  		 printf("%d ",j);
+	  }
+	  printf("\n");
+  }
 }
