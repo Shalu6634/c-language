e908356a2c2869bf7985007aeec995f6a69e388c
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2023-12-26 18:29:43 +0530

    Add files via upload

---
+++ udf/string len.c
@@ -0,0 +1,16 @@
+//Q.2 Write a Program to find the length of the String by passing a string as an argument using UDF.
+#include<stdio.h>
+void string(int a)
+{
+	int b;
+    int length=strlen(b);
+    printf("length is :%d",length);
+    return string;
+}
+main()
+{
+	char n[50],a;
+	printf("enter your name:");
+	gets(n);
+	string(a);
+}
